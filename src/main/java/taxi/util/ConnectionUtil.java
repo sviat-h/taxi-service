8706fada656251d97219f2e1084eceb55af95aa1
sviat-h <svyatoslav.grinik@gmail.com>
2022-03-31 19:49:15 +0300

    code was refactored

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,11 +6,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi?useUnicode=true"
-            + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "YOUR_URL";
+    private static final String USERNAME = "YOUR_USERNAME";
+    private static final String PASSWORD = "YOUR_PASSWORD";
+    private static final String JDBC_DRIVER = "YOUR_JDBC_DRIVER";
 
     static {
         try {
